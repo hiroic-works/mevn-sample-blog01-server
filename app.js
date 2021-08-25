e9f77e4af5163ae6c592e5c25ce363f798884f4a
h.oda <62193961+hiroic-works@users.noreply.github.com>
2021-08-25 11:06:34 +0900

    サーバーセットアップ

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Hello');
+});
+
+app.listen(port, () => console.log(`Server started http://localhost:${port}`));
